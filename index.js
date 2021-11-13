026657a1b789bfe6fe93e745ba5ce7c740125c15
ManoLo2ManoLo <manuel_canas@aol.com>
2021-11-13 15:01:54 -0500

    added comments and error functions

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ function askQuestion() {
 
 // function to display departments
 function displayDepartments() {
+
     // mySQL Call to View Department ID and Name
     const sql = `SELECT department.id, department.name AS department FROM department`
     db.query(sql, (err, results) => {
@@ -70,6 +71,7 @@ function displayDepartments() {
 
 // function to display roles
 function displayRoles() {
+
     // mySQL Call to View Role ID, Title, Department, and Salary
     const sql = `SELECT role.id, role.title, department.name AS department, role.salary
                 FROM role JOIN department ON role.department_id = department.id`
@@ -84,9 +86,10 @@ function displayRoles() {
 
 // function to display to display employees
 function displayEmployees() {
+
     // mySQL Call to View Employee ID, First Name, Last Name, Role Title, Department, Salary, and Manager
     const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title,
-                        department.name AS department, role.salary, manager.first_name AS manager
+                        department.name AS department, role.salary, manager.first_name AND manager.last_name AS manager
                 FROM employee
                 JOIN role ON employee.role_id = role.id
                 JOIN department ON role.department_id = department.id
@@ -110,6 +113,7 @@ function addDepartment() {
             validate: input => { if (input && input.length <= 30) { return true; } else { return false; }}
         }
     ]).then(function(data) {
+
         // function input Department Name into Department Table
         db.query(`INSERT INTO department (name) VALUES ('${data.department}')`, (err) => {
             if (err) throw err;
@@ -121,11 +125,13 @@ function addDepartment() {
 
 // function to add a Role
 function addRole() {
+
     // mySQL call to get Department Names;
     db.query(`Select department.name FROM department`, (err, results) => {
         if (err) throw err;
 
         let departmentArr = [];
+
         // for loop to print department names into an array
         for(let i = 0; i < results.length; i++) {
             departmentArr.push(results[i].name)
@@ -151,6 +157,7 @@ function addRole() {
                 choices: departmentArr
             }
         ]).then(function(data) {
+
             // mySQL call to ID from Department Name
             db.query(`SELECT * FROM department WHERE  name = '${data.department}'`, (err, results) => {
                 if (err) throw err;
@@ -166,18 +173,27 @@ function addRole() {
     });
 }
 
-// Employee's first name, last name, role, and manager
+// funciton to add employee's first name, last name, role, and manager
 function addEmployee() {
+
+    // mySQL call to get roles list
     db.query(`SELECT role.title FROM role`, (err, data1) => {
+        if (err) throw err;
+
         let roleArr = [];
 
+        // for loop to insert roles into an array
         for (let i = 0; i < data1.length; i++) {
             roleArr.push(data1[i].title)
         }
 
+        // mySQL call to get an employee list to select manager
         db.query(`SELECT employee.first_name, employee.last_name FROM employee`, (err, data2) => {
+            if (err) throw err;
+
             let managerArr = [];
 
+            // for loop ot insert employee(manager) into an array
             for (let i = 0; i < data2.length; i++) {
                 let manager = `${data2[i].first_name} ${data2[i].last_name}`
                 managerArr.push(manager)
@@ -188,25 +204,13 @@ function addEmployee() {
                     type:'input',
                     name:'firstName',
                     message:"What is your new employee's first name?",
-                    validate: input => {
-                        if (input && input.length <= 30) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
+                    validate: input => { if (input && input.length <= 30) { return true;} else { return false; }}
                 },
                 {
                     type:'input',
                     name:'lastName',
                     message:"What is your new employee's last name?",
-                    validate: input => {
-                        if (input && input.length <= 30) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
+                    validate: input => { if (input && input.length <= 30) { return true; } else { return false; }}
                 },
                 {
                     type:'list',
@@ -232,8 +236,12 @@ function addEmployee() {
                 let managerfirstname = managername[0];
                 let managerlastname = managername[1];
 
+                // mySQL call to get employee's id based on employee selected from the array
                 const sql1 = `SELECT id FROM role WHERE title = '${data3.role}' UNION SELECT id FROM employee WHERE first_name = '${managerfirstname}' AND last_name = '${managerlastname}'`
                 db.query(sql1,(err, data4) => {
+                    if (err) throw err;
+
+                    // mySQL call to insert employee first name, last name, role, and manager into employee table
                     let sql2 = '';
                     if (data4.length === 1) {
                         sql2 = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -242,21 +250,27 @@ function addEmployee() {
                         sql2 = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
                                 VALUES ('${data3.firstName}','${data3.lastName}','${data4[0].id}','${data4[1].id}')`
                     }
-                    
                     db.query(sql2, (err) => {
                         if (err) throw err;
 
                         askQuestion();
-                    })
+                    });
                 });
-            })
+            });
         });
     });
 }
 
+// function to update an employee's role
 function updateEmployeeRole() {
+
+    // mySQL call to selection employee first and last name for an array
     db.query(`SELECT employee.first_name, employee.last_name FROM employee`, (err, data1) => {
+        if (err) throw err;
+
         let employeeArr = [];
+
+        // for loop to create an array of employees
         for (let i = 0; i < data1.length; i++) {
             let employee = `${data1[i].first_name} ${data1[i].last_name}`
             employeeArr.push(employee)
@@ -275,12 +289,18 @@ function updateEmployeeRole() {
             let employeefirstname = employeename[0];
             let employeelastname = employeename[1];
 
+            // mySQL call to get roles for an array
             db.query(`SELECT role.title FROM role`, (err, data3) => {
+                if (err) throw err;
+
                 let roleArr = [];
+
+                // for loop to create an array of roles
                 for (let i = 0; i < data3.length; i++) {
                     let role = data3[i].title
                     roleArr.push(role)
                 }
+
                 inquirer.prompt([
                     {
                         type:'list',
@@ -289,28 +309,34 @@ function updateEmployeeRole() {
                         choices: roleArr
                     }
                 ]).then(function(results) {
+
+                    //mySQL call that will role id based on user selection
                     db.query(`SELECT id FROM role WHERE title = '${results.role}'`, (err, moreresult) => {
-                        moreSQL = `UPDATE employee
+                        if (err) throw err;
+
+                        //mySQL call that will update a user role base on first and last name
+                        const sql = `UPDATE employee  
                                     SET role_id = '${moreresult[0].id}'
                                     WHERE first_name = '${employeefirstname}' AND last_name ='${employeelastname}'`
-
-                        db.query(moreSQL, (err) => {
+                        db.query(sql, (err) => {
                             if (err) throw err;
 
                             askQuestion();
-                        })
-                    })
-                })
+                        });
+                    });
+                });
 
-            })
-        })
+            });
+        });
 
-    })
+    });
 }
 
+// connects to mySQL
 db.connect(err => {
     if (err) throw err;
     console.log()
 })
 
+// starts the applicaiton
 initialize();
