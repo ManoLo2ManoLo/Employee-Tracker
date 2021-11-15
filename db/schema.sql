8b793a5dc26764e14067c3f16cb22d7c8feb820b
ManoLo2ManoLo <manuel_canas@aol.com>
2021-11-14 19:41:49 -0500

    fixed minor issue on shcema

--- db/schema.sql
+++ db/schema.sql
@@ -7,15 +7,15 @@ CREATE TABLE employee (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id int,
-    manager_id int
+    role_id INTEGER,
+    manager_id INTEGER
 );
 
 CREATE TABLE role (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2) NOT NULL,
-    department_id int
+    department_id INTEGER
 );
 
 CREATE TABLE department (
